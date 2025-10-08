dce9d0ee6ee83673e7c401b958a7af6cac934db0
Divnich 💖 <divna.mijic@gmail.com>
2025-10-07 20:26:09 -0400

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -37,7 +37,7 @@ const About: React.FC = () => {
             </a>
           </div>
           <img
-            src="media/images/about-me.png"
+            src="/media/images/about-me.png"
             alt="me"
             className="h-[60vh]"
           />
